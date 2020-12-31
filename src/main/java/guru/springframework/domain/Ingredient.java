7e649bca18afefc2ab3d4a551e5f7fd7d16426c8
Lena <lena@email.com>
2020-12-31 14:39:20 +0100

    added unit of measure

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -20,6 +20,9 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
+    @OneToOne
+    private UnitOfMeasure unitOfMeasure;
+
     public Long getId() {
         return id;
     }
---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,15 @@
+package guru.springframework.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String uom;
+}
